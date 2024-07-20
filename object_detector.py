67e559f589613cbd850e46385b25944aab413863
ThanmaiGR <thanmaigr2@gmail.com>
2024-07-20 12:33:10 +0530

    added face recog

--- object_detector.py
+++ object_detector.py
@@ -5,34 +5,36 @@ import cvzone
 import numpy as np
 import os
 from yolo_cam.eigen_cam import EigenCAM
-from yolo_cam.utils.image import show_cam_on_image, scale_cam_image
+from yolo_cam.utils.image import show_cam_on_image
 import math
+import check_valid
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
 
-def video():
+def video(salience=False):
     # This function captures video from a webcam or a video file and performs object detection on the frames.
 
     # For capturing video from the webcam
-    cap = cv2.VideoCapture(0)
-    cap.set(3, 640)  # Set width
-    cap.set(4, 480)  # Set height
+    # cap = cv2.VideoCapture(0)
+    # cap.set(3, 1920)  # Set width
+    # cap.set(4, 1080)  # Set height
 
     # For capturing video from a file
-    # cap = cv2.VideoCapture(".mp4")
+    cap = cv2.VideoCapture(r"Videos for testing/video3.mp4")
 
-    return detect(cap)
+    return detect(cap, salience)
 
 
-def detect(cap):
+def detect(cap, salience=False):
     # Infinite loop to process each frame from the video stream
     while True:
         # Capture the current frame from the video source
         success, img = cap.read()  # 'success' indicates if a frame was successfully captured
 
         if success:
+            image = cv2.resize(img, (640, 640))
             # Perform object detection on the captured frame
             results = model(img, stream=True)  # 'results' contains detected objects' information
 
@@ -44,22 +46,33 @@ def detect(cap):
                     x1, y1, x2, y2 = box.xyxy[0]
                     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                     w, h = x2 - x1, y2 - y1
-
+                    cropped_img = img[y1:y2, x1:x2]
                     # Extract confidence (accuracy) and class label
                     conf = math.ceil((box.conf[0] * 100)) / 100
                     cls = int(box.cls[0])
-                    # Display the object's class name and confidence level on the frame
-                    cvzone.putTextRect(img, f'{class_names[cls]} {conf}', (max(0, x1), max(30, y1)))
+                    label = "intruder"
+                    if class_names[cls] == 'student':
+                        label = check_valid.recognize_student(cropped_img)
+                    if class_names[cls] == 'faculty':
+                        label = check_valid.recognize_faculty(cropped_img)
+                    # # Display the object's class name and confidence level on the frame
+                    cvzone.putTextRect(image, f'{label} {conf}', (max(0, x1), max(30, y1)))
 
                     # Draw a rectangle around the detected object
                     cvzone.cornerRect(img, (x1, y1, w, h))
 
             # Display the processed frame with object detection
-            cv2.imshow("Image", img)
-
+            if salience:
+                rgb_img = cv2.resize(image, (640, 640))
+                img = cv2.resize(img, (640, 640))
+                sal_img = saliency(rgb_img)
+                final_image = cv2.hconcat([img, sal_img])
+            else:
+                final_image = img
+                final_image = cv2.resize(final_image, (2560, 1600))
+            cv2.imshow("Image", final_image)
             # Wait for a key press to prevent the video from closing immediately
             cv2.waitKey(1)
-            cv2.destroyAllWindows()
         else:
             break  # Break the loop if there are no more frames or an error occurs
 
@@ -70,7 +83,7 @@ def saliency(rgb_img):
     target_layers =[model.model.model[-4]]
     cam = EigenCAM(model, target_layers, task='od')
     grayscale_cam = cam(rgb_img)[0, :, :]
-    cam_image = show_cam_on_image(image, grayscale_cam, use_rgb=True)
+    cam_image = show_cam_on_image(image, grayscale_cam, use_rgb=False)
     # plt.imshow(cam_image)
     # plt.show()
     return cam_image
@@ -80,6 +93,7 @@ def predict_for_folder(folder_path, salience=False):
     # Infinite loop to process each frame from the video stream
     for image_name in os.listdir(folder_path):
         image = cv2.imread(folder_path + image_name)
+        image = cv2.resize(image, (640, 640))
         img = image.copy()
         # Perform object detection on the captured frame
         results = model(image) # 'results' contains detected objects' information
@@ -91,13 +105,17 @@ def predict_for_folder(folder_path, salience=False):
                 x1, y1, x2, y2 = box.xyxy[0]
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                 w, h = x2 - x1, y2 - y1
-
+                cropped_img = img[y1:y2, x1:x2]
                 # Extract confidence (accuracy) and class label
                 conf = math.ceil((box.conf[0] * 100)) / 100
                 cls = int(box.cls[0])
-                # Display the object's class name and confidence level on the frame
-                cvzone.putTextRect(image, f'{class_names[cls]} {conf}', (max(0, x1), max(30, y1)))
-
+                label = "intruder"
+                if class_names[cls] == 'student':
+                    label = check_valid.recognize_student(cropped_img)
+                if class_names[cls] == 'faculty':
+                    label = check_valid.recognize_faculty(cropped_img)
+                # # Display the object's class name and confidence level on the frame
+                cvzone.putTextRect(image, f'{label} {conf}', (max(0, x1), max(30, y1)))
                 # Draw a rectangle around the detected object
                 cvzone.cornerRect(image, (x1, y1, w, h))
 
@@ -116,13 +134,13 @@ def predict_for_folder(folder_path, salience=False):
         cv2.destroyAllWindows()
 
 
-model = YOLO('bestv2.pt')
-class_names = ["intruder", "student",]
-path = r"../human_intrusion_data_2/test/images/"
+model = YOLO('best.pt')
+class_names = ["intruder", "student", "faculty"]
+path = r"./img for human intrusion/train/images/"
 
 
 # path = r"images-for-testing/"
 # print(model.eval())
 
 predict_for_folder(path, salience=True)
-
+video()
